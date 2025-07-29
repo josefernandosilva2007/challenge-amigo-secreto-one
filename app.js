6ae8c0f729c604fd3109a0db1a5363685d694f7e
josefernando <josefernandosilva0501@gmail.com>
2025-07-29 13:42:51 -0700

    feat: function sort friend

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let list = document.getElementById('result');
+list.innerHTML = "";
 let nameFriends = [];
 
 function addFriend(){
@@ -9,3 +11,14 @@ function addFriend(){
         nameFriends.push(input);
     }
 }
+
+function sortFriend(){
+    let indexSorted = parseInt(Math.random() * nameFriends.length);
+    let sortedFriend = nameFriends.at(indexSorted);
+    console.log(sortedFriend);
+    list.innerHTML = `<li>${sortedFriend}</li>`;
+}
+
+
+
+
