0fca24cb3e2e83ebf67d98c80fa73e0f19e1c92a
josefernando <josefernandosilva0501@gmail.com>
2025-07-29 13:09:54 -0700

    feat: fuction add friend

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let nameFriends = [];
+
+function addFriend(){
+    let input = document.querySelector('input').value;
+    if(input == ''){
+        alert('Por favor digite um nome valido');
+    } else {
+        nameFriends.push(input);
+    }
+}
